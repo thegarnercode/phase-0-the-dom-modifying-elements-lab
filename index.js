b0c515db48894783d0c724d9c69dc7de0b41b607
Denisha Garner <thegarnercode@gmail.com>
2022-04-20 03:05:27 -0400

    commit tree 2289088727b2f1c4fbfc313a91c1dfe15adf4d35

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+// const element = document.createElement("div")
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+  //const li = document.createElement("li");
+  //li.textContent = (i + 1).toString();
+  //ul.append(li);
+//}
+
+//element.append(ul);
+
+main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Denisha is the champion";
+
+element.append(main);
